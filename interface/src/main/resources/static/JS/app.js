78bfea85d7e7ebb7df3140d5e6295c635db6741a
aquel69 <alexandre.lardon@yahoo.fr>
2021-12-01 09:38:40 +0100

    ajout des pages albums + contact + ajout des icones de lien externe

--- interface/src/main/resources/static/JS/app.js
+++ interface/src/main/resources/static/JS/app.js
@@ -1,6 +1,7 @@
 // Navigation bar color change
 
 let nav = document.getElementById('navigation');
+let liens_icon = document.getElementsByClassName('lien_nav_icone');
 let liens = document.getElementsByClassName('liensNav');
 let icon = document.getElementById('icon');
 
@@ -28,6 +29,10 @@ function colorHeader() {
                 liens[3].style.color = "#373737";
                 liens[4].style.color = "#373737";
 
+                liens_icon[0].style.color = "#373737";
+                liens_icon[1].style.color = "#373737";
+                liens_icon[2].style.color = "#373737";
+
             } else {
                 nav.style.background = "#8A2BE2FF";
                 liens[0].style.color = "#f7f7f7";
@@ -35,6 +40,10 @@ function colorHeader() {
                 liens[2].style.color = "#f7f7f7";
                 liens[3].style.color = "#f7f7f7";
                 liens[4].style.color = "#f7f7f7";
+
+                liens_icon[0].style.color = "#f7f7f7";
+                liens_icon[1].style.color = "#f7f7f7";
+                liens_icon[2].style.color = "#f7f7f7";
             }
 
             // second else pour mettre la nav en clair et bloqué l'anim du dessus
@@ -46,6 +55,10 @@ function colorHeader() {
             liens[2].style.color = "#373737";
             liens[3].style.color = "#373737";
             liens[4].style.color = "#373737";
+
+            liens_icon[0].style.color = "#373737";
+            liens_icon[1].style.color = "#373737";
+            liens_icon[2].style.color = "#373737";
         }
     }
 
