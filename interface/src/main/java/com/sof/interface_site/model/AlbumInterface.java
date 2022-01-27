2020039e1afb130f9a42073915a043f1dd568593
aquel69 <alexandre.lardon@yahoo.fr>
2022-01-27 17:53:15 +0100

    ajout de la responsivité pour les écrans en 1920px + bug sur classe Album interface problème lombok les data ne se générent pas

--- interface/src/main/java/com/sof/interface_site/model/AlbumInterface.java
+++ interface/src/main/java/com/sof/interface_site/model/AlbumInterface.java
@@ -3,8 +3,6 @@ package com.sof.interface_site.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
 public class AlbumInterface {
 
     /**
@@ -77,4 +75,115 @@ public class AlbumInterface {
      */
     private String tarifEP;
 
+    public int getIdAlbumInterface() {
+        return idAlbumInterface;
+    }
+
+    public void setIdAlbumInterface(int idAlbumInterface) {
+        this.idAlbumInterface = idAlbumInterface;
+    }
+
+    public String getSousTitre1() {
+        return sousTitre1;
+    }
+
+    public void setSousTitre1(String sousTitre1) {
+        this.sousTitre1 = sousTitre1;
+    }
+
+    public String getSousTitre2() {
+        return sousTitre2;
+    }
+
+    public void setSousTitre2(String sousTitre2) {
+        this.sousTitre2 = sousTitre2;
+    }
+
+    public String getPhotoAlbum() {
+        return photoAlbum;
+    }
+
+    public void setPhotoAlbum(String photoAlbum) {
+        this.photoAlbum = photoAlbum;
+    }
+
+    public String getPhotoCD() {
+        return photoCD;
+    }
+
+    public void setPhotoCD(String photoCD) {
+        this.photoCD = photoCD;
+    }
+
+    public String getChanson1() {
+        return chanson1;
+    }
+
+    public void setChanson1(String chanson1) {
+        this.chanson1 = chanson1;
+    }
+
+    public String getChanson2() {
+        return chanson2;
+    }
+
+    public void setChanson2(String chanson2) {
+        this.chanson2 = chanson2;
+    }
+
+    public String getChanson3() {
+        return chanson3;
+    }
+
+    public void setChanson3(String chanson3) {
+        this.chanson3 = chanson3;
+    }
+
+    public String getChanson4() {
+        return chanson4;
+    }
+
+    public void setChanson4(String chanson4) {
+        this.chanson4 = chanson4;
+    }
+
+    public String getTitreChanson1() {
+        return titreChanson1;
+    }
+
+    public void setTitreChanson1(String titreChanson1) {
+        this.titreChanson1 = titreChanson1;
+    }
+
+    public String getTitreChanson2() {
+        return titreChanson2;
+    }
+
+    public void setTitreChanson2(String titreChanson2) {
+        this.titreChanson2 = titreChanson2;
+    }
+
+    public String getTitreChanson3() {
+        return titreChanson3;
+    }
+
+    public void setTitreChanson3(String titreChanson3) {
+        this.titreChanson3 = titreChanson3;
+    }
+
+    public String getTitreChanson4() {
+        return titreChanson4;
+    }
+
+    public void setTitreChanson4(String titreChanson4) {
+        this.titreChanson4 = titreChanson4;
+    }
+
+    public String getTarifEP() {
+        return tarifEP;
+    }
+
+    public void setTarifEP(String tarifEP) {
+        this.tarifEP = tarifEP;
+    }
 }
